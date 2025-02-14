c9ad795cfcdc866131dab3302770fe420fa9684e
Azis <azis@Aziss-MacBook-Pro.local>
2025-02-14 16:36:41 +0700

    dsdsd

---
+++ App.js
@@ -0,0 +1,60 @@
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+
+export default function App() {
+  const checkForUpdates = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        Alert.alert(
+          "Update Tersedia",
+          "Ada pembaruan baru! Ingin mengupdate sekarang?",
+          [
+            {
+              text: "Nanti",
+              style: "cancel",
+            },
+            {
+              text: "Update Sekarang",
+              onPress: async () => {
+                const { isNew } = await Updates.fetchUpdateAsync();
+                if (isNew) {
+                  await Updates.reloadAsync(); // Restart aplikasi untuk apply update
+                }
+              },
+            },
+          ]
+        );
+      } else {
+        console.log("Tidak ada update tersedia.");
+      }
+    } catch (error) {
+      console.error("Gagal memeriksa pembaruan:", error);
+    }
+  };
+
+  useEffect(() => {
+    checkForUpdates();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app! azis ganteng sekali lagi
+      </Text>
+      <Button title="Update" color={"red"} onPress={checkForUpdates()} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
